c92ff3c8159529c56987b6a59294f41ac8fcc36a
Joao V. S. Santos <87675230+JustAnotherRedFox@users.noreply.github.com>
2024-05-27 20:22:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,71 +39,17 @@ reflector \
 
  pacman -Syy
 
+#-----------------------------------
+# Packages and FStab
+#-----------------------------------
+
 # install necessary package
 pacstrap /mnt base base-devel linux linux-lts linux-firmware grub efibootmgr sudo os-prober intel-ucode
 
 # Generate an fstab config
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# change root into new system
-arch-chroot /mnt
+# move sh to root
+cp -a *.sh /mnt/root/
 
 
-
-# uncomment locale setting
-sed -i -e 's|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|' /etc/locale.gen 
-
-# generate locale conf
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# set local zone
-ln -sf /usr/share/zoneinfo/America/Bahia /etc/localtime
-hwclock --systohc
-
-# set host name
-echo "JustAFox" > /etc/hostname
-
-echo "127.0.0.1     localhost" > /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1     JustAFox.localdomain   JustAFox" >> /etc/hosts
-
-# set root password
-passwd
-
-
-# nvim /etc/pacman.conf
-
-# adding to mirror list to allow use of 32bit app on 64bit app
-echo "[multilib]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-
-# add mirrorlist for Arch User Repositories(AUR)
-echo "[archlinuxfr]" >> /etc/pacman.conf
-echo "SigLevel = Never" >> /etc/pacman.conf
-echo "Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
-
-# update all packeges
-pacman -Syu
-
-
-
-# install and configure grub
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
-grub-mkconfig -o /boot/grub/grub.cfg
-
-
-
-# Create user account
-useradd -m -g users -G wheel JustAFox
-
-# setting password for user
-passwd JustAFox
-
-# adding users to list of sudo
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-# unmount partitions
-umount /mnt/boot /mnt
-
-echo "Please reboot the device and execute postinstall.sh"
